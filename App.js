b32845c9a6aac8808a7d402695170b21e6dd9e35
Ing Leonardo <lrmaldo@gmail.com>
2020-04-28 09:24:42 -0500

    cambios en el tabbar

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 // import Components
 import Tienda from './src/detalle_tienda/tienda'
 import Carrito from './src/detalle_tienda/carrito'
@@ -27,7 +28,7 @@ import Datos_tienda from './src/detalle_tienda/datos_tienda'
 
 //import de inicio
 import Inicio from './src/inicio/inicio'
-import perfil from './src/inicio/perfil'
+import Perfil from './src/inicio/perfil'
 
 // unable console yellow
 console.disableYellowBox = true;
@@ -38,13 +39,134 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 
 
-const TabNavigator = createBottomTabNavigator({
-   Inicio: Inicio,
-    Perfil: perfil,
-  //Perfil:Perfil
+
+
+const TabIconInicio = (props) => (
+  <Icon name="md-home" size={30} color={props.focused ? 'white' : 'darkgrey'} />
+  
+)
+const TabIconPerfil = (props) => (
+  <Icon name="md-person" size={30} color={props.focused ? 'white' : 'darkgrey'} />
+  
+)
+
+const HomeNavigator = createStackNavigator({
+  Home:{
+    screen: Inicio,
+    navigationOptions:{
+          title:"Marketux",
+          headerTitleAlign:'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+        headerTintColor:"white",
+        headerBackground:<View>
+        <View style={{height:60, backgroundColor:"#007bff", position:'absolute', top:0, left:0, right:0, borderBottomRightRadius:0}}/>
+        <View style={{height:55, backgroundColor:"#c51d34", position:'absolute', top:0, left:0, right:0, borderBottomRightRadius:0}}/>
+      </View>,
+
+      
+        
+        
+    },
+  },
+  
+  
 });
 
-export default createAppContainer(TabNavigator);
+
+const PerfilNavigator = createStackNavigator({
+  Perfil:{
+    screen: Perfil,
+    navigationOptions:{
+          title:"Perfil",
+          headerTitleAlign:'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+        headerTintColor:"white",
+        headerBackground:<View>
+        <View style={{height:60, backgroundColor:"#007bff", position:'absolute', top:0, left:0, right:0, borderBottomRightRadius:0}}/>
+        <View style={{height:55, backgroundColor:"#c51d34", position:'absolute', top:0, left:0, right:0, borderBottomRightRadius:0}}/>
+      </View>,
+
+      
+        
+        
+    },
+  },
+  
+  
+});
+
+const BottonNavegation = createBottomTabNavigator({
+  
+  Home:{
+    screen:HomeNavigator,
+    navigationOptions:{
+      title:'Inicio',
+      tabBarIcon: TabIconInicio,
+     // tabBarIcon: ({activeTintColor}) => <FontAwesome name="home" color={"white"}></FontAwesome>
+    },
+    
+       
+  },
+  Perfil:{
+    screen: PerfilNavigator,
+    navigationOptions:{
+      title:'Perfil',
+      tabBarIcon: TabIconPerfil,
+     // tabBarIcon: ({activeTintColor}) => <FontAwesome name="home" color={"white"}></FontAwesome>
+    },
+  },
+  
+  /**Notificacion: {
+    screen:NotificacionNavigator,
+    navigationOptions:{
+      title:'Notificacion',
+      tabBarIcon: TabIconNotificaciones,
+    },
+   
+  },
+  */
+ 
+
+
+  
+  
+  
+ 
+
+ 
+ /* screen:AgendaNavigator,
+  screen:NotificacionNavigator,
+  screen: GeoMapsNavigator,
+  screen: PerfilNavigator,
+  #007bff
+*/
+}
+,
+
+{tabBarOptions: {
+  activeTintColor: 'white',
+  //inactiveTintColor:'#6c757d',
+  labelStyle: {
+    fontSize: 12,
+  },
+  style: {
+    backgroundColor: '#c51d34',
+  },
+}
+}
+
+)
+
+
+export default  createAppContainer(BottonNavegation)
+
+
+
+
 
 /** 
  
